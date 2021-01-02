25b0c464a9bb843f841c080d263f17ee0ab1c9a6
Andrea Cardiota <andreacardiota@gmail.com>
2021-01-02 19:14:54 +0100

    add semi-final Signup.js and Login.js design

--- app/src/components/user/authentication/Login.js
+++ app/src/components/user/authentication/Login.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import {Box, Fab, Grid, TextField, Typography} from "@material-ui/core";
+import SendIcon from '@material-ui/icons/Send';
+import {makeStyles} from "@material-ui/core/styles";
+
+const backgroundImage = "authLanding.jpeg";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundImage: `url(${(backgroundImage)})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        flexGrow: 1
+    },
+    buttonStyle: {
+        color: "red",
+    },
+    title: {
+        color: "black",
+        fontSize: 49,
+        fontFamily: [
+            'Roboto', 'sans-serif'
+        ].join(',')
+    },
+    textfieldGrid: {
+        padding: theme.spacing(5)
+    }
+}));
 
 function Login() {
+    const classes = useStyles();
     return (
-
-        <div></div>
+        <Box height="100vh" display="flex" flexDirection="column" className={classes.root}>
+            <Box py={10}>
+                <Grid container direction="column" alignItems="center">
+                    <Grid>
+                        <Typography>
+                            <Box className={classes.title}>
+                                Log in
+                            </Box>
+                        </Typography>
+                    </Grid>
+                    <Grid className={classes.textfieldGrid} item container direction="row" xs={10} md={7} lg={5}>
+                        <TextField required={true} fullWidth={true} variant="filled" label="Email o username"/>
+                        <TextField required={true} fullWidth={true} variant="filled" label="Password"/>
+                    </Grid>
+                    <Fab color={"primary"}
+                         disabled={false}
+                         href={""}
+                         iconTheme={"Filled"}
+                         size={"large"}
+                         variant={"round"}>
+                        <SendIcon />
+                    </Fab>
+                </Grid>
+            </Box>
+        </Box>
     );
 }
 
--- app/src/components/user/authentication/Signup.js
+++ app/src/components/user/authentication/Signup.js
@@ -1,31 +1,63 @@
 import React from 'react';
-import {Fab, Grid, TextField, Typography} from "@material-ui/core";
+import {Box, Fab, Grid, TextField, Typography} from "@material-ui/core";
 import SendIcon from '@material-ui/icons/Send';
+import {makeStyles} from "@material-ui/core/styles";
+
+const backgroundImage = "authLanding.jpeg";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundImage: `url(${(backgroundImage)})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        flexGrow: 1
+    },
+    buttonStyle: {
+        color: "red",
+    },
+    title: {
+        color: "black",
+        fontSize: 49,
+        fontFamily: [
+            'Roboto', 'sans-serif'
+        ].join(',')
+    },
+    textfieldGrid: {
+        padding: theme.spacing(5)
+    }
+}));
 
 function Signup() {
+    const classes = useStyles();
     return (
-        <Grid container direction="column" alignItems="center">
-            <Grid>
-                <Typography variant="h3">
-                    Sign up
-                </Typography>
-            </Grid>
-            <Grid item container direction="row">
-                <TextField fullWidth={true} variant="filled" label="Email *"/>
-                <TextField fullWidth={true} variant="filled" label="Username *"/>
-                <TextField fullWidth={true} variant="filled" label="Password *"/>
-                <TextField fullWidth={true} variant="filled" label="Conferma password *"/>
-            </Grid>
-            <Fab color={"secondary"}
-                 label={""}
-                 disabled={false}
-                 href={""}
-                 iconTheme={"Filled"}
-                 size={"large"}
-                 variant={"round"}>
-                <SendIcon />
-            </Fab>
-        </Grid>
+        <Box height="100vh" display="flex" flexDirection="column" className={classes.root}>
+            <Box py={10}>
+                <Grid container direction="column" alignItems="center">
+                    <Grid>
+                        <Typography>
+                            <Box className={classes.title}>
+                                Sign up
+                            </Box>
+                        </Typography>
+                    </Grid>
+                    <Grid className={classes.textfieldGrid} item container direction="row" xs={10} md={7} lg={5}>
+                        <TextField required={true} fullWidth={true} variant="filled" label="Email" />
+                        <TextField required={true} fullWidth={true} variant="filled" label="Username"/>
+                        <TextField required={true} fullWidth={true} variant="filled" label="Password"/>
+                        <TextField required={true} fullWidth={true} variant="filled" label="Conferma password"/>
+                    </Grid>
+                    <Fab color={"primary"}
+                         disabled={false}
+                         href={""}
+                         iconTheme={"Filled"}
+                         size={"large"}
+                         variant={"round"}>
+                        <SendIcon />
+                    </Fab>
+                </Grid>
+            </Box>
+        </Box>
     );
 }
 
