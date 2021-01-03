574244dd30934aca12c8a0c3ec15f9cf1e540d36
Lorenzo Rizzato <lorenzo.rizzato@studio.unibo.it>
2021-01-03 18:05:04 +0100

    implemented GET /users

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,3 +1,10 @@
+const User = require('../models/User')
+
 exports.userList = async function(req, res) {
-    res.send('not yet implemented')
+    try {
+        const users = await User.find();
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json({ message: err })
+    }
 }
