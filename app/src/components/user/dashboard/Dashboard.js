edb486bca700da21a8cd166088be2a9665c0eca0
Andrea Cardiota <andreacardiota@gmail.com>
2021-01-03 13:01:32 +0100

    add Personal.js initial front-end design

--- app/src/components/user/dashboard/Dashboard.js
+++ app/src/components/user/dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+
+}));
 
 function Dashboard() {
-
+    const classes = useStyles();
     return (
         <div></div>
     );
--- app/src/components/user/personal/Personal.js
+++ app/src/components/user/personal/Personal.js
@@ -1,8 +1,178 @@
 import React from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {Grid} from "@material-ui/core";
 
-function Personal(props) {
+const useStyles = makeStyles(theme => ({
+    root: {
+        maxWidth: 345,
+    },
+    mediaPersonal: {
+        height: 100,
+        width: '33%',
+        paddingTop: '60%', // 16:9
+        marginLeft: '30%'
+    },
+    mediaTarget: {
+        height: 100,
+        width: '40%',
+        paddingTop: '50%', // 16:9
+        marginLeft: '28%'
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+}));
+
+function Personal() {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return (
-        <div></div>
+        <Grid container spacing={4} justify="center">
+            <Grid item xs={7} md={4} lg={3}>
+                <Card className={classes.root}>
+                    <CardHeader
+                        avatar={
+                            <Avatar aria-label="personalAvatar" className={classes.avatar}>
+                                AC
+                            </Avatar>
+                        }
+                        action={
+                            <IconButton aria-label="settings">
+                                <MoreVertIcon />
+                            </IconButton>
+                        }
+                        title="Info"
+                        subheader="Le tue informazioni personali"
+                    />
+                    <CardMedia
+                        className={classes.mediaPersonal}
+                        image="personRunning.png"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Espandendo questa scheda troverai tutte le tue informazioni personale, quali: username, età, altezza e peso.
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                        </IconButton>
+                        <IconButton aria-label="share">
+                            <ShareIcon />
+                        </IconButton>
+                        <IconButton
+                            className={clsx(classes.expand, {
+                                [classes.expandOpen]: expanded,
+                            })}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>Username:</Typography>
+                            <Typography paragraph>Username from DB</Typography>
+                            <Typography paragraph>Age:</Typography>
+                            <Typography paragraph>Age from DB</Typography>
+                            <Typography paragraph>Height:</Typography>
+                            <Typography paragraph>Height from DB</Typography>
+                            <Typography paragraph>Weight:</Typography>
+                            <Typography paragraph>Weight from DB</Typography>
+                        </CardContent>
+                    </Collapse>
+                </Card>
+            </Grid>
+
+            <Grid item xs={7} md={4} lg={3}>
+                <Card className={classes.root}>
+                    <CardHeader
+                        action={
+                            <IconButton aria-label="settings">
+                                <MoreVertIcon />
+                            </IconButton>
+                        }
+                        title="Perdi peso"
+                        subheader="I tuoi obiettivi personali"
+                    />
+                    <CardMedia
+                        className={classes.mediaTarget}
+                        image="fire.png"
+                        title="Paella dish"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Espandendo questa scheda troverai i tuoi obiettivi di fitness, quali: peso target, indice di massa grassa target,
+                            calorie giornaliere e numero di allenamenti settianali previsti dalla tua scheda.
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                        </IconButton>
+                        <IconButton aria-label="share">
+                            <ShareIcon />
+                        </IconButton>
+                        <IconButton
+                            className={clsx(classes.expand, {
+                                [classes.expandOpen]: expanded,
+                            })}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>Peso target:</Typography>
+                            <Typography paragraph>Peso target from DB</Typography>
+                            <Typography paragraph>Indice di massa grassa target:</Typography>
+                            <Typography paragraph>Indice di massa grassa target from DB</Typography>
+                            <Typography paragraph>Calorie giornaliere da assumere:</Typography>
+                            <Typography paragraph>Calorie giornaliere da assumere from DB</Typography>
+                            <Typography paragraph>Allenamenti settimanali:</Typography>
+                            <Typography paragraph>Allenamenti settimanali from DB</Typography>
+                        </CardContent>
+                    </Collapse>
+                </Card>
+            </Grid>
+
+        </Grid>
+
     );
 }
 
