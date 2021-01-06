835fe6db87b39f53a01ce619cb82bdb4c5747134
luca giulianini <luca.giulianini2@studio.unibo.it>
2021-01-06 21:37:32 +0100

    Fixed all envs

--- api/src/Server.js
+++ api/src/Server.js
@@ -6,9 +6,7 @@ const path = require('path')
 const privateKey  = fs.readFileSync(path.resolve(__dirname, './security/cert.key'), 'utf8');
 const certificate = fs.readFileSync(path.resolve(__dirname, './security/cert.pem'), 'utf8');
 
-if (!process.env.DOCKER) { // Check if inside a container
-    require('dotenv').config({ path: path.resolve(__dirname, '../../.local.env') })
-}
+require('dotenv').config({path: path.resolve(__dirname, '../.env')})
 
 console.log(process.env.DOCKER)
 
