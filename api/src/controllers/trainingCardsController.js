8540eb1335a6ebfd19737ca86e4981eea9c22b7e
Lorenzo Rizzato <lorenzo.rizzato@studio.unibo.it>
2021-01-09 12:27:27 +0100

    implement card creation

--- api/src/controllers/trainingCardsController.js
+++ api/src/controllers/trainingCardsController.js
@@ -1,7 +1,17 @@
-const TrainingCard = require('../models/TrainingCard')
 const responses = require('./util/responses')
 
 const User = require('../models/User')
+const Exercise = require('../models/Exercise')
+const TrainingCard = require('../models/TrainingCard')
+
+async function getExerciseIds(exercises, res) {
+    const exerciseDocs = await Exercise.find()
+        .where('name')
+        .in(exercises)
+        .select('_id')
+        .exec()
+    return exerciseDocs.map(doc => doc._id)
+}
 
 exports.createTrainingCard = async function(req, res) {
     const user = req.body.user
@@ -11,8 +21,6 @@ exports.createTrainingCard = async function(req, res) {
     const userExists = await User.exists({ username: user })
     if (!userExists) {
         return responses.badRequest(res)('User does not exist')
-    } else {
-        console.log('user exists')
     }
 
     const trainerExists = await User.exists({ username: trainer })
@@ -20,7 +28,30 @@ exports.createTrainingCard = async function(req, res) {
         return responses.badRequest(res)('Trainer does not exist')
     }
 
-    console.log(exercises)
+    const exerciseIds = await getExerciseIds(exercises.map(obj => obj.exercise))
+    if (exerciseIds.length !== exercises.length) {
+        return responses.badRequest(res)('Some exercises do not exist')
+    }
 
-    res.sendStatus(200)
+    const userId = await User.findOne({ username: user }).map(doc => doc._id).exec();
+    const trainerId = await User.findOne({ username: user }).map(doc => doc._id).exec();
+    const exercisesWithIds = await Promise.all(exercises.map(async obj => {
+        obj.exercise = await Exercise.findOne({ name: obj.exercise })
+            .map(doc => doc._id)
+            .exec()
+        return obj
+    }))
+
+    const trainingCard = new TrainingCard({
+        user: userId,
+        trainer: trainerId,
+        exercises: exercisesWithIds
+    })
+
+    try {
+        const savedTrainingCard = await trainingCard.save()
+        responses.created(res)(savedTrainingCard)
+    } catch(err) {
+        responses.error(res)(err)
+    }
 }
