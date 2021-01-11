76beabe56f43a61fb74eb34d8a8cb56542acd667
luca giulianini <luca.giulianini2@studio.unibo.it>
2021-01-11 13:51:21 +0100

    Finishing header

--- app/src/components/user/training/TrainingBar.js
+++ app/src/components/user/training/TrainingBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Button, Chip, Container, Grid, LinearProgress, Typography} from "@material-ui/core";
+import {Button, Chip, Grid, LinearProgress, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import {Grade} from "@material-ui/icons";
 
 const useStyles = makeStyles({
-
+    root: {
+        margin: 0,
+        padding: 0,
+    },
     titleText: {
         fontWeight: 100,
         fontSize: 30,
@@ -17,16 +20,18 @@ const useStyles = makeStyles({
         marginRight: 5,
         marginBottom: 5,
     },
-    playButton: {}
+    playButton: {},
 })
 
 
 function TrainingBar(props) {
     const classes = useStyles()
     return (
-        <Container color={"white"}>
-            <Typography className={classes.titleText}> {props.title}</Typography>
-            <Grid container direction={"row"} justify={"flex-start"} alignItems={"center"} wrap={"wrap"}>
+        <Grid container className={classes.root}>
+            <Grid item>
+                <Typography className={classes.titleText}> {props.title}</Typography>
+            </Grid>
+            <Grid container item direction={"row"} justify={"flex-start"} alignItems={"center"} wrap={"wrap"}>
                 {props.badges.map(chip => {
                     return (
                         <Grid item>
@@ -39,22 +44,23 @@ function TrainingBar(props) {
                     )
                 })}
             </Grid>
-            <Grid container direction={"column"} wrap={"wrap"}>
-                <Grid item container direction={"row"} justify={"space-between"} alignItems={"center"}>
-                    <Grid item>
-                        <Typography className={classes.timerText}>
-                            Time: {props.trainingTime}
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Button variant={"outlined"} className={classes.playButton}>Start</Button>
-                    </Grid>
+            <Grid item container direction={"row"} justify={"space-between"} alignItems={"center"}>
+                <Grid item>
+                    <Typography className={classes.timerText}>
+                        Time: {props.trainingTime}
+                    </Typography>
                 </Grid>
-                <Grid item xl={12}>
-                    <LinearProgress variant="determinate" value={10} color="secondary"/>
+                <Grid item>
+                    <Button variant={"outlined"} className={classes.playButton}>Start</Button>
                 </Grid>
             </Grid>
-        </Container>
+            <Grid container item>
+                <Grid item xs={12}>
+                    <LinearProgress variant="determinate" value={50}
+                                    color="secondary"/>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
 
