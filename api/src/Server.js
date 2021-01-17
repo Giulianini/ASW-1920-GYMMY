2540e243cf0fab5dc17c44382fed3a5befe69211
Lorenzo Rizzato <lorenzo.rizzato@studio.unibo.it>
2021-01-17 17:40:27 +0100

    set port on socket.io cors config

--- api/src/Server.js
+++ api/src/Server.js
@@ -22,7 +22,7 @@ const httpsServer = https.createServer(credentials, app);
 
 const io = require('socket.io')(httpServer, {
     cors: {
-        origin: "http://localhost",
+        origin: "http://localhost:8080",
         methods: [ "GET", "POST" ],
         credentials: true
     }
