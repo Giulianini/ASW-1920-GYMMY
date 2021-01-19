560b1fa4926aba22212ab08c821b105b98b1e52a
luca giulianini <luca.giulianini2@studio.unibo.it>
2021-01-19 11:54:12 +0100

    Stats update

--- app/src/components/user/statistics/Statistics.js
+++ app/src/components/user/statistics/Statistics.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import {Container, Grid} from "@material-ui/core";
+import React, {useState} from 'react';
 import {makeStyles} from "@material-ui/core/styles";
-import Chart from "react-apexcharts";
+import {Container, Grid, withStyles} from "@material-ui/core";
+import {AreaSeries, ArgumentAxis, Chart, Legend, Title, ValueAxis,} from '@devexpress/dx-react-chart-material-ui';
+import {Animation} from '@devexpress/dx-react-chart';
 
 const useStyles = makeStyles(theme => ({
-    rootGrid:{
+    rootGrid: {
         [theme.breakpoints.up('md')]: {
             height: "95vh",
         },
@@ -15,208 +16,85 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
-const completedActivities = {
-    options: {
-        xaxis: {
-            categories: ['January', 'February', 'March',
-                'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        },
-        title: {
-            text: 'Completed activities',
-            align: 'center',
-            floating: true,
-            style: {
-                fontSize:    '20px',
-                fontWeight:  'normal',
-                fontFamily:  'Roboto',
-                color:       'black'
-            },
-        },
-        subtitle: {
-            text: '(Monthly)',
-            align: 'center',
-            floating: false,
-            style: {
-                fontSize:    '13px',
-                fontWeight:  'normal',
-                fontFamily:  'Roboto',
-                color:       'black'
-            },
-        }
+const chartRootStyles = {
+    chart: {
+        paddingRight: '20px',
     },
-    series: [
-        {
-            data: [30, 40, 31, 30, 49, 50, 28, 27, 33, 39, 45, 51]
-        }
-    ]
 };
+const legendStyles = {
+    root: {
+        display: 'flex',
+        margin: 'auto',
+        flexDirection: 'row',
+    },
+};
+const legendLabelStyles = theme => ({
+    label: {
+        paddingTop: theme.spacing(1),
+    },
+});
+const legendItemStyles = {
+    item: {
+        flexDirection: 'column',
+    },
+};
+const ChartRootBase = ({classes, ...restProps}) => (
+    <Chart.Root {...restProps} className={classes.chart}/>
+);
+const LegendRootBase = ({classes, ...restProps}) => (
+    <Legend.Root {...restProps} className={classes.root} children={<div/>}/>
+);
+const LegendLabelBase = ({classes, ...restProps}) => (
+    <Legend.Label {...restProps} className={classes.label}/>
+);
+const LegendItemBase = ({classes, ...restProps}) => (
+    <Legend.Item {...restProps} className={classes.item} children={<div/>}/>
+);
 
-const workoutTime = {
-    options: {
-        xaxis: {
-            categories: ['January', 'February', 'March',
-                'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        },
-        title: {
-            text: 'Workout time',
-            align: 'center',
-            floating: true,
-            style: {
-                fontSize:    '20px',
-                fontWeight:  'normal',
-                fontFamily:  'Roboto',
-                color:       'black'
-            },
-        },
-        subtitle: {
-            text: '(minutes)',
-            align: 'center',
-            floating: false,
-            style: {
-                fontSize:    '13px',
-                fontWeight:  'normal',
-                fontFamily:  'Roboto',
-                color:       'black'
-            },
-        },
-        dataLabels: {
-            enabled: true,
-            textAnchor: 'middle',
-            distributed: false,
-            offsetX: 0,
-            offsetY: 0,
-            style: {
-                fontSize: '10px',
-                fontFamily: 'Roboto',
-                fontWeight: 'normal',
-            },
-            background: {
-                enabled: true,
-                foreColor: '#fff',
-                padding: 4,
-                borderRadius: 5,
-                borderWidth: 1,
-                borderColor: '#fff',
-                opacity: 0.9,
-                dropShadow: {
-                    enabled: true,
-                    top: 3,
-                    left: 3,
-                    blur: 5,
-                    color: '#000',
-                    opacity: 0.5
-                }
-            },
-        }
-    },
-    series: [
-        {
-            data: [15, 31, 47, 66, 75, 93, 112, 130, 149, 169, 190, 202]
-        }
-    ]
-};
+const ChartRoot = withStyles(chartRootStyles, {name: 'ChartRoot'})(ChartRootBase);
+const LegendRoot = withStyles(legendStyles, {name: 'LegendRoot'})(LegendRootBase);
+const LegendLabel = withStyles(legendLabelStyles, {name: 'LegendLabel'})(LegendLabelBase);
+const LegendItem = withStyles(legendItemStyles, {name: 'LegendItem'})(LegendItemBase);
 
-const distanceCovered = {
-    options: {
-        xaxis: {
-            categories: ['January', 'February', 'March',
-                'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        },
-        title: {
-            text: 'Distance covered',
-            align: 'center',
-            floating: true,
-            style: {
-                fontSize:    '20px',
-                fontWeight:  'normal',
-                fontFamily:  'Roboto',
-                color:       'black'
-            },
-        },
-        subtitle: {
-            text: '(km)',
-            align: 'center',
-            floating: false,
-            style: {
-                fontSize:    '13px',
-                fontWeight:  'normal',
-                fontFamily:  'Roboto',
-                color:       'black'
-            },
-        },
-        dataLabels: {
-            enabled: true,
-            textAnchor: 'middle',
-            distributed: false,
-            offsetX: 0,
-            offsetY: 0,
-            style: {
-                fontSize: '10px',
-                fontFamily: 'Roboto',
-                fontWeight: 'normal',
-            },
-            background: {
-                enabled: true,
-                foreColor: '#fff',
-                padding: 4,
-                borderRadius: 5,
-                borderWidth: 1,
-                borderColor: '#fff',
-                opacity: 0.9,
-                dropShadow: {
-                    enabled: true,
-                    top: 3,
-                    left: 3,
-                    blur: 5,
-                    color: '#000',
-                    opacity: 0.5
-                }
-            },
-        }
-    },
-    series: [
-        {
-            data: [15, 31, 18, 22, 44, 55, 32, 38, 28, 35, 43, 36]
-        }
-    ]
-};
 
 function Statistics() {
     const classes = useStyles();
+    const [data, setData] = useState([
+        {year: '2012', android: 10, ios: 11},
+        {year: '2015', android: 12, ios: 13},
+    ])
     return (
         <Container maxWidth={"lg"}>
             <Grid container direction={"column"} alignContent={"center"} className={classes.rootGrid}>
                 <Grid item>
-                    <div>
-                        <Chart
-                            options={completedActivities.options}
-                            series={completedActivities.series}
-                            type="bar"
-                            width="370"
-                        />
-                    </div>
-                </Grid>
+                    <Chart
+                        data={data}
+                        rootComponent={ChartRoot}
+                    >
+                        <ArgumentAxis/>
+                        <ValueAxis/>
 
-                <Grid item>
-                    <div>
-                        <Chart
-                            options={distanceCovered.options}
-                            series={distanceCovered.series}
-                            type="area"
-                            width="365"
+                        <AreaSeries
+                            name="Android"
+                            valueField="android"
+                            argumentField="year"
                         />
-                    </div>
-                </Grid>
-
-                <Grid item>
-                    <div>
-                        <Chart
-                            options={workoutTime.options}
-                            series={workoutTime.series}
-                            type="area"
-                            width="365"
+                        <AreaSeries
+                            name="iOS"
+                            valueField="ios"
+                            argumentField="year"
                         />
-                    </div>
+                        <Animation/>
+                        <Legend
+                            position="bottom"
+                            rootComponent={LegendRoot}
+                            itemComponent={LegendItem}
+                            labelComponent={LegendLabel}
+                        />
+                        <Title
+                            text="Worldwide Sales to End Users by OS"
+                        />
+                    </Chart>
                 </Grid>
             </Grid>
         </Container>
