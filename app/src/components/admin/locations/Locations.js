f61be871749fe533f81a8ac9d57d76b7f62c1e07
luca giulianini <luca.giulianini2@studio.unibo.it>
2021-01-29 11:30:50 +0100

    Added popup in locations

--- app/src/components/admin/locations/Locations.js
+++ app/src/components/admin/locations/Locations.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Fab, Grid, Slider, TextField, Typography} from "@material-ui/core";
+import {Fab, Grid, Popover, Slider, TextField, Typography} from "@material-ui/core";
 import {useSnackbar} from "notistack";
 import {makeStyles} from "@material-ui/core/styles";
 import {baseAxios} from "../../../Api";
-import {Add} from "@material-ui/icons";
+import {Add, Info} from "@material-ui/icons";
+import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles({
     form: {
@@ -14,6 +15,9 @@ const useStyles = makeStyles({
     },
     gridItem: {
         width: "100%",
+    },
+    gridBlock: {
+        marginTop: 20
     },
     sliderText: {
         fontWeight: 100,
@@ -34,10 +38,22 @@ const useStyles = makeStyles({
 function Locations() {
     const classes = useStyles()
     const {enqueueSnackbar} = useSnackbar()
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
     const [values, setValues] = React.useState({
         description: '',
         defaultCapacity: 0,
     })
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
 
     const handleChange = (prop, value) => {
         setValues({
@@ -74,8 +90,30 @@ function Locations() {
             <Grid item>
                 <Typography variant={"h5"} className={classes.title}>
                     Insert location
+                    <IconButton aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
+                        <Info/>
+                    </IconButton>
                 </Typography>
             </Grid>
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
+                <Typography className={classes.typography}>
+                    A location is considered as a place, a room or a machine where a single or more users can train.
+                    Example of location are: Chest-Press machine, Stretching Area or Zumba Room.
+                </Typography>
+            </Popover>
             <Grid item container xs={10} md={5} direction={"column"}>
                 <Grid item className={classes.gridItem}>
                     <TextField
@@ -89,7 +127,7 @@ function Locations() {
                         label="Location name"
                         variant="standard"/>
                 </Grid>
-                <Grid item container direction={"column"} alignItems={"flex-start"}>
+                <Grid item container direction={"column"} alignItems={"flex-start"} className={classes.gridBlock}>
                     <Grid item>
                         <Typography className={classes.sliderTitle}>Default capacity</Typography>
                     </Grid>
