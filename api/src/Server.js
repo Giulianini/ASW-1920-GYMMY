5fa47634d88dabc2dd4c9267d0fe2d15a126c7e6
luca giulianini <luca.giulianini2@studio.unibo.it>
2021-01-31 16:10:56 +0100

    Finished replication and all working

--- api/src/Server.js
+++ api/src/Server.js
@@ -101,7 +101,7 @@ mongoose.connect(
         user: dbAdmin,
         pass: dbPassword,
         useNewUrlParser: true,
-        //useUnifiedTopology: true //Comment for connection to docker mongo instance. (from outside)
+        useUnifiedTopology: true //Comment for connection to docker mongo instance. (from outside)
     },
     (err, db) => {
         if (err) {
--- mongo/etc/initReplicaSet.js
+++ mongo/etc/initReplicaSet.js
@@ -1,4 +1,4 @@
-rs.initiate( {
+rs.status().ok || rs.initiate( {
     _id : "rs0",
     members: [
         { _id: 0, host: "mongo1" },
